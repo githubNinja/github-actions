b0f6c66e3e8502d98d457e77b331a9e0b3e9b78b
kd91076 <DharanikotaKranth>
2022-02-28 11:35:35 -0600

    echos

--- scripts/raise_pr.sh
+++ scripts/raise_pr.sh
@@ -15,8 +15,11 @@ echo Input1 ${MODULE_TO_BUMP}
 echo Input2 ${VERSION_TO_BUMP}
 echo Input3 ${GITHUB_ACCESS_TOKEN}
 git clone https://github.com/githubNinja/${MODULE_TO_BUMP}.git
+echo "current dir::" `pwd`
 mvn versions:set -DnewVersion=${VERSION_TO_BUMP}
+echo list here `ls`
 git checkout -b bump-githubNinja-patch-1
+echo list here2 `ls`
 git add pom.xml
 git commit -m "chore:bump ${MODULE_TO_BUMP}"
 git push origin bump-githubNinja-patch-1
