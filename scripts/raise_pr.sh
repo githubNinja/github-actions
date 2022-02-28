e24243ac4b487fbe52622a513b4f272e8ccd1c53
kd91076 <DharanikotaKranth>
2022-02-28 11:13:35 -0600

    newer version

--- scripts/raise_pr.sh
+++ scripts/raise_pr.sh
@@ -6,10 +6,11 @@ VERSION_TO_BUMP=$2
 GITHUB_ACCESS_TOKEN=$3
 echo "Exporting path New:::"
 #export JAVA_HOME="/c/Program Files/Amazon Corretto/jdk1.8.0_252/bin"
+export JAVA_HOME="/mnt/c/jdk1.8.0_252/bin"
 echo "Exporting path2:::" $JAVA_HOME
 
 #export PATH=$PATH:$JAVA_HOME/bin
-echo "version::" ${java -version}
+java -version
 echo Input1 ${MODULE_TO_BUMP}
 echo Input2 ${VERSION_TO_BUMP}
 echo Input3 ${GITHUB_ACCESS_TOKEN}
@@ -18,10 +19,8 @@ mvn versions:set -DnewVersion=${VERSION_TO_BUMP}
 git checkout -b bump-githubNinja-patch-1
 git add pom.xml
 git commit -m "chore:bump ${MODULE_TO_BUMP}"
-#git push origin bump-${{inputs.module_to_bump}}
 git push origin bump-githubNinja-patch-1
 curl  -X POST -H "Content-Type: application/vnd.github.v3+json" -H "Accept:application/vnd.github.v3+json"  -H 'Authorization: token ${GITHUB_ACCESS_TOKEN}' https://api.github.com/repos/githubNinja/github-actions/pulls -d '{ "title": "'Bump::github-actions::1.0.1-SNAPSHOT'", "body": "Auto Bump Pull Request.", "head": "bump-githubNinja-patch-1", "base": "main" }'
-#echo Hello ${{ inputs.who-to-greet }}
 ls
 echo executing the bash script awesome !!
-#bash ./good_morning.sh
+bash ./good_morning.sh
